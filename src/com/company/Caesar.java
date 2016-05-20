692cbe26b6a4512d63e63c1c9aa6489c1084fa6a
OlegVlasenko <vlasenko.inbox@gmail.com>
2016-05-21 02:55:29 +0300

    + Caesar

---
+++ src/com/company/Caesar.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.PrimitiveIterator;
+
+public class Caesar {
+
+    private static final int CODE_OFFSET = 11;
+    private static final int V_SIZE = 73;
+
+    public static void encodeArray(ArrayList<String> paramArray) {
+        for (String Item : paramArray) {
+            Item = encodeValue(Item);
+        }
+    }
+
+     private static String encodeValue(String input) {
+
+        StringBuilder res = new StringBuilder();
+
+        PrimitiveIterator.OfInt cpIterator = input.codePoints().iterator();
+
+        while (cpIterator.hasNext()) {
+            int cp = cpIterator.next();
+            res.appendCodePoint('1' + Math.floorMod(cp - '1' + CODE_OFFSET, V_SIZE));
+        }
+
+        return res.toString();
+    }
+
+    public static void  decodeArray(ArrayList<String> paramArray) {
+        for (String Item : paramArray) {
+            Item = decodeValue(Item);
+        }
+    }
+
+    private static String decodeValue(String input) {
+
+        StringBuilder res = new StringBuilder();
+
+        PrimitiveIterator.OfInt cpIterator = input.codePoints().iterator();
+
+        while (cpIterator.hasNext()) {
+            int cp = cpIterator.next();
+            res.appendCodePoint('1' + Math.floorMod(cp - '1' - CODE_OFFSET, V_SIZE));
+        }
+
+        return res.toString();
+    }
+
+}
